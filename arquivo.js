2bc24727f25b6a62605593ddebdaa50e8fceace0
felpzsccp <123281130+felpzsccp@users.noreply.github.com>
2023-03-08 19:02:56 -0300

    Add files via upload

---
+++ arquivo.js
@@ -0,0 +1,5 @@
+var nome = prompt('Digite o seu nome')
+var idade = prompt('Digite a sua idade')
+
+
+document.write('<h1> Olá ' + nome + ', tudo bem? Estou vendo que você possui '+ idade  +' anos</h1>')
